600ebbf888862776e957ef20b6abe52f7da3bb4b
Fady Emad <60940882+fady2019@users.noreply.github.com>
2023-10-25 15:38:59 +0300

    the getAverage function is created, it's used to calc the avg of numbers in a vector

--- main.cpp
+++ main.cpp
@@ -7,11 +7,17 @@ int getSum(vector<int> nums)
     return accumulate(nums.begin(), nums.end(), 0);
 }
 
+double getAverage(vector<int> nums)
+{
+    return getSum(nums) / ((double)nums.size());
+}
+
 int main(int argc, char const *argv[])
 {
     vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     cout << "Sum = " << getSum(nums) << endl;
+    cout << "Avg = " << getAverage(nums) << endl;
 
     return 0;
 }
