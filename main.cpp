e8a44a01705e3b7e2a4579f8c377c8c824013b2b
Fady Emad <60940882+fady2019@users.noreply.github.com>
2023-10-25 15:55:23 +0300

    the getMin function is created, it's used to get the min number in a vector

--- main.cpp
+++ main.cpp
@@ -7,11 +7,17 @@ int getSum(vector<int> nums)
     return accumulate(nums.begin(), nums.end(), 0);
 }
 
+int getMin(vector<int> nums)
+{
+    return *min_element(nums.begin(), nums.end());
+}
+
 int main(int argc, char const *argv[])
 {
     vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     cout << "Sum = " << getSum(nums) << endl;
+    cout << "Min = " << getMin(nums) << endl;
 
     return 0;
 }
